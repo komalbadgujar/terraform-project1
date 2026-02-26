2cf20b9e6e5089505a757841d8f9c77eb1716423
komalbadgujar <komalbadgujar1057@gmail.com>
2026-02-26 16:02:49 +0530

    added a file

---
+++ main.tf
@@ -0,0 +1,167 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+####################
+# VPC
+####################
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+####################
+# Subnets
+####################
+
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+}
+
+####################
+# Internet Gateway
+####################
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+####################
+# Route Table
+####################
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+####################
+# Security Group
+####################
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+####################
+# Load Balancer
+####################
+
+resource "aws_lb" "alb" {
+  name               = "ha-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_sg.id]
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+}
+
+####################
+# Target Group
+####################
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+####################
+# Listener
+####################
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+####################
+# Launch Template
+####################
+
+resource "aws_launch_template" "web_template" {
+  name_prefix   = "web-template"
+  image_id      = "ami-051a31ab2f4d498f5" # Amazon Linux 2 (ap-south-1)
+  instance_type = "t2.micro"
+
+  user_data = base64encode(<<-EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "Hello from $(hostname)" > /var/www/html/index.html
+EOF
+  )
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+
+####################
+# Auto Scaling Group
+####################
+
+resource "aws_autoscaling_group" "web_asg" {
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 2
+  vpc_zone_identifier = [aws_subnet.public1.id, aws_subnet.public2.id]
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
+
+  launch_template {
+    id      = aws_launch_template.web_template.id
+    version = "$Latest"
+  }
+}
